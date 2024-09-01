78f5e6bf6be36be3282def5f00b54618a82715da
AlexisAR666 <alex0507845@gmail.com>
2024-09-01 14:27:41 -0600

    Create pares 0-100.c

---
+++ pares 0-100.c
@@ -0,0 +1,23 @@
+/* Aguirre Rojas Alexis
+Programa pares de 0 a 100*/
+#include <stdio.h>
+#include <math.h>
+
+int i;
+
+int main ()
+{
+   printf ("Los numeros pares entre 0 y 100 son:  ");
+
+   for (i = 0; i <= 100; i++)
+   {
+      if (i % 2 == 0)
+      {
+         printf (" %d ", i);
+      }
+   }
+
+   printf ("\n");
+   return 0;
+}
+    
